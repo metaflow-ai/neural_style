d08a34aefba1cbdd9b3b577cd9d94eb0e95862a2
Morgan Giraud <morgan@explee.com>
2016-05-11 18:25:37 +0200

    Use MS cocoset for training/bal/test data

--- data/download_images.sh
+++ data/download_images.sh
@@ -1,1 +1,4 @@
-cat urls.txt | awk '{ print $2 }' | xargs wget --no-check-certificate -t 1 --timeout=5 -P ./train/
+# cat urls.txt | awk '{ print $2 }' | xargs wget --no-check-certificate -t 1 --timeout=5 -P ./train/
+head -1 data/urls.txt | xargs wget --no-check-certificate -t 1 --timeout=5 -P ./train/
+head -2 data/urls.txt | tail -1 | xargs wget --no-check-certificate -t 1 --timeout=5 -P ./val/
+head -3 data/urls.txt | tail -1 | xargs wget --no-check-certificate -t 1 --timeout=5 -P ./test/
