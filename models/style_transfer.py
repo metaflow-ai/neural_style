e76fe4dbfc8aef87ca3c8b159c0bfb74a660435d
Morgan Giraud <morgan@explee.com>
2016-06-15 16:40:35 +0200

    Add model style_transfer_conv_inception_3

--- models/style_transfer.py
+++ models/style_transfer.py
@@ -204,6 +204,7 @@ def style_transfer_conv_inception_2(weights_path=None, input_shape=(3, 600, 600)
 
     return model
 
+# Less capacity than the inception "en serie"
 def style_transfer_conv_inception_2_parallel(weights_path=None, input_shape=(3, 600, 600), nb_res_layer=6):
     input = Input(shape=input_shape, name='input', dtype='float32')
 
@@ -275,62 +276,51 @@ def style_transfer_conv_inception_3(weights_path=None, input_shape=(3, 600, 600)
     input = Input(shape=input_shape, name='input', dtype='float32')
 
     # Downsampling
-    c = Convolution2D(29, 3, 3, 
+    c = Convolution2D(13, 3, 3, 
         init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(input)
-    bn11 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
-    a11 = Activation('relu')(bn11)
+    bn11 = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c)
+    a11 = PReLU()(bn11) 
     mp11 = MaxPooling2D(pool_size=(2, 2), border_mode='same')(input)
-    m = merge([a11, mp11], mode='concat', concat_axis=1) # 32 layers
+    m = merge([a11, mp11], mode='concat', concat_axis=1) # 16 layers
 
-    c12 = Convolution2D(96, 3, 3, 
+    c12 = Convolution2D(48, 3, 3, 
         init='he_normal', subsample=(2, 2),  border_mode='same', activation='linear')(m)
-    bn12 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c12)
-    a12 = Activation('relu')(bn12)
+    bn12 = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c12)
+    a12 = PReLU()(bn12)
     mp12 = MaxPooling2D(pool_size=(2, 2), border_mode='same')(m)
     m = merge([a12, mp12], mode='concat', concat_axis=1) # 64 layers
 
-    c13 = Convolution2D(256, 3, 3, 
+    c13 = Convolution2D(128, 3, 3, 
         init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(m)
-    bn13 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c13)
-    last_out = Activation('relu')(bn13)
+    bn13 = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c13)
+    last_out = PReLU()(bn13)
 
     for i in range(nb_res_layer):
         #bottleneck archi
-        c = Convolution2D(64, 1, 1, 
+        c = Convolution2D(32, 1, 1, 
             init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(last_out)
-        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
-        a = Activation('relu')(bn)
+        bn = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c)
+        a = PReLU()(bn)
 
         # Convolutions
-        c = Convolution2D(64, 3, 3, 
+        c = Convolution2D(32, 3, 3, 
             init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a)
-        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
-        a = Activation('relu')(bn)
-        c = Convolution2D(64, 3, 3, 
+        bn = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c)
+        a = PReLU()(bn)
+        c = Convolution2D(32, 3, 3, 
             init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a)
-        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+        bn = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c)
         a = Activation('relu')(bn)
 
         #Reverse bottleneck
-        c = Convolution2D(256, 1, 1, 
+        c = Convolution2D(128, 1, 1, 
             init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a)
-        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+        bn = BatchNormalization(axis=1, momentum=0.9, gamma_init='he_normal')(c)
         last_out = merge([last_out, bn], mode='sum')
-        last_out = ScaledSigmoid(scaling=255.)(last_out)
 
-    ct71 = ConvolutionTranspose2D(128, 3, 3, 
-        init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(last_out)
-    bn71 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(ct71)
-    a71 = Activation('relu')(bn71)
-    
-    ct81 = ConvolutionTranspose2D(32, 3, 3, 
-        init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(a71)
-    bn81 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(ct81)
-    a81 = Activation('relu')(bn81)    
-
-    c = Convolution2D(3, 3, 3, 
-        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a81)
-    out = ScaledSigmoid(scaling=255.)(c)
+    ct = ConvolutionTranspose2D(3, 5, 5, 
+        init='he_normal', subsample=(4, 4), border_mode='same', activation='linear')(last_out)
+    out = ScaledSigmoid(scaling=255.)(ct)
 
     model = Model(input=[input], output=[out])
 
@@ -431,25 +421,29 @@ if __name__ == "__main__":
     dir = os.path.dirname(os.path.realpath(__file__))
     resultsDir = dir + '/data/st'
 
-    model = style_transfer_conv_transpose()
-    export_model(model, resultsDir + '/style_transfer_conv_transpose')
-    plot_model(model, resultsDir + '/style_transfer_conv_transpose/model.png', True)
+    # model = style_transfer_conv_transpose()
+    # export_model(model, resultsDir + '/style_transfer_conv_transpose')
+    # plot_model(model, resultsDir + '/style_transfer_conv_transpose/model.png', True)
 
-    model = style_transfer_conv_inception()
-    export_model(model, resultsDir + '/style_transfer_conv_inception')
-    plot_model(model, resultsDir + '/style_transfer_conv_inception/model.png', True)
+    # model = style_transfer_conv_inception()
+    # export_model(model, resultsDir + '/style_transfer_conv_inception')
+    # plot_model(model, resultsDir + '/style_transfer_conv_inception/model.png', True)
 
-    model = style_transfer_conv_inception_2()
-    export_model(model, resultsDir + '/style_transfer_conv_inception_2')
-    plot_model(model, resultsDir + '/style_transfer_conv_inception_2/model.png', True)
+    # model = style_transfer_conv_inception_2()
+    # export_model(model, resultsDir + '/style_transfer_conv_inception_2')
+    # plot_model(model, resultsDir + '/style_transfer_conv_inception_2/model.png', True)
 
-    model = style_transfer_conv_inception_2_parallel()
-    export_model(model, resultsDir + '/style_transfer_conv_inception_2_parallel')
-    plot_model(model, resultsDir + '/style_transfer_conv_inception_2_parallel/model.png', True)
+    # model = style_transfer_conv_inception_2_parallel()
+    # export_model(model, resultsDir + '/style_transfer_conv_inception_2_parallel')
+    # plot_model(model, resultsDir + '/style_transfer_conv_inception_2_parallel/model.png', True)
 
     model = style_transfer_conv_inception_3()
     export_model(model, resultsDir + '/style_transfer_conv_inception_3')
     plot_model(model, resultsDir + '/style_transfer_conv_inception_3/model.png', True)
+
+    model = style_transfer_conv_inception_3(nb_res_layer=1)
+    export_model(model, resultsDir + '/style_transfer_conv_inception_3_1reslayer')
+    plot_model(model, resultsDir + '/style_transfer_conv_inception_3_1reslayer/model.png', True)
 
     # model = style_transfer_conv_inception_ELU_flattened()
     # export_model(model, resultsDir + '/style_transfer_conv_inception_ELU_flattened')
