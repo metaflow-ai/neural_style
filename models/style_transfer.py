193632f6883c0aab1362f7a3c566f49c6bc7cf37
Morgan Giraud <morgan@explee.com>
2016-06-15 10:49:47 +0200

    Add ELU flattened model

--- models/style_transfer.py
+++ models/style_transfer.py
@@ -4,9 +4,9 @@ sys.path.append(dir + '/..')
 
 from keras import backend as K
 from keras.engine import merge
-from keras.layers.convolutional import (Convolution2D, MaxPooling2D, UpSampling2D)
+from keras.layers.convolutional import (Convolution2D, MaxPooling2D, AveragePooling2D, UpSampling2D)
 from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import PReLU
+from keras.layers.advanced_activations import PReLU, ELU
 from keras.layers.core import Activation
 from keras.layers import Input
 from keras.models import Model
@@ -70,15 +70,16 @@ def style_transfer_conv_transpose(weights_path=None, input_shape=(3, 600, 600), 
 
     return model
 
-def style_transfer_conv_inception(weights_path=None, input_shape=(3, 600, 600), nb_res_layer=12):
+# Moving from 6 to 12 layers doesn't seem to improve much
+def style_transfer_conv_inception(weights_path=None, input_shape=(3, 600, 600), nb_res_layer=6):
     input = Input(shape=input_shape, name='input', dtype='float32')
 
     # Downsampling
-    c11 = Convolution2D(13, 3, 3, 
+    c = Convolution2D(13, 3, 3, 
         init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(input)
-    c11 = Convolution2D(13, 3, 3, 
-        init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(c11)
-    bn11 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c11)
+    c = Convolution2D(13, 3, 3, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+    bn11 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
     a11 = Activation('relu')(bn11)
     mp11 = MaxPooling2D(pool_size=(2, 2), border_mode='same')(input)
     m = merge([a11, mp11], mode='concat', concat_axis=1) # 16 layers
@@ -128,11 +129,92 @@ def style_transfer_conv_inception(weights_path=None, input_shape=(3, 600, 600), 
     bn81 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(ct81)
     a81 = Activation('relu')(bn81)    
 
-    c = Convolution2D(16, 3, 3, 
-        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a81)
     c = Convolution2D(3, 3, 3, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a81)
+    out = Convolution2D(3, 3, 3, 
         init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
-    out = ScaledSigmoid(scaling=255.)(c)
+    out = ScaledSigmoid(scaling=255.)(out)
+
+    model = Model(input=[input], output=[out])
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
+
+def style_transfer_conv_inception_ELU_flattened(weights_path=None, input_shape=(3, 600, 600), nb_res_layer=6):
+    input = Input(shape=input_shape, name='input', dtype='float32')
+
+    # Downsampling
+    c = Convolution2D(13, 3, 3, 
+        init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(input)
+    c = Convolution2D(13, 1, 3, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+    c = Convolution2D(13, 3, 1, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+    bn11 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+    a11 = ELU()(bn11)
+    mp11 = AveragePooling2D(pool_size=(2, 2), border_mode='same')(input)
+    m = merge([a11, mp11], mode='concat', concat_axis=1) # 16 layers
+
+    c12 = Convolution2D(48, 3, 3, 
+        init='he_normal', subsample=(2, 2),  border_mode='same', activation='linear')(m)
+    bn12 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c12)
+    a12 = ELU()(bn12)
+    mp12 = AveragePooling2D(pool_size=(2, 2), border_mode='same')(m)
+    m = merge([a12, mp12], mode='concat', concat_axis=1) # 64 layers
+
+    c = Convolution2D(128, 3, 1, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(m)
+    c = Convolution2D(128, 1, 3, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+    bn13 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+    last_out = ELU()(bn13)
+
+    for i in range(nb_res_layer):
+        #bottleneck archi
+        c = Convolution2D(32, 1, 1, 
+            init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(last_out)
+        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+        a = ELU()(bn)
+
+        # Convolutions
+        c = Convolution2D(32, 1, 5, 
+            init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a)
+        c = Convolution2D(32, 5, 1, 
+            init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+        a = ELU()(bn)
+        c = Convolution2D(32, 1, 5, 
+            init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a)
+        c = Convolution2D(32, 5, 1, 
+            init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+        a = ELU()(bn)
+
+        #Reverse bottleneck
+        c = Convolution2D(128, 1, 1, 
+            init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a)
+        bn = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(c)
+        last_out = merge([last_out, bn], mode='sum')
+
+    ct71 = ConvolutionTranspose2D(64, 3, 3, 
+        init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(last_out)
+    bn71 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(ct71)
+    a71 = ELU()(bn71)
+    
+    ct81 = ConvolutionTranspose2D(16, 3, 3, 
+        init='he_normal', subsample=(2, 2), border_mode='same', activation='linear')(a71)
+    bn81 = BatchNormalization(axis=1, momentum=0.1, gamma_init='he_normal')(ct81)
+    a81 = ELU()(bn81)    
+
+    c = Convolution2D(3, 3, 3, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(a81)
+    c = Convolution2D(3, 3, 1, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+    out = Convolution2D(3, 1, 3, 
+        init='he_normal', subsample=(1, 1), border_mode='same', activation='linear')(c)
+    out = ScaledSigmoid(scaling=255.)(out)
 
     model = Model(input=[input], output=[out])
 
@@ -220,15 +302,19 @@ if __name__ == "__main__":
     dir = os.path.dirname(os.path.realpath(__file__))
     resultsDir = dir + '/data/st'
 
-    model = style_transfer_conv_transpose()
-    export_model(model, resultsDir + '/style_transfer_conv_transpose')
-    plot_model(model, resultsDir + '/style_transfer_conv_transpose/model.png', True)
+    # model = style_transfer_conv_transpose()
+    # export_model(model, resultsDir + '/style_transfer_conv_transpose')
+    # plot_model(model, resultsDir + '/style_transfer_conv_transpose/model.png', True)
 
-    model = style_transfer_conv_inception()
-    export_model(model, resultsDir + '/style_transfer_conv_inception')
-    plot_model(model, resultsDir + '/style_transfer_conv_inception/model.png', True)
+    # model = style_transfer_conv_inception()
+    # export_model(model, resultsDir + '/style_transfer_conv_inception')
+    # plot_model(model, resultsDir + '/style_transfer_conv_inception/model.png', True)
 
-    model = style_transfer_enet_inspired()
-    export_model(model, resultsDir + '/style_transfer_enet_inspired')
-    plot_model(model, resultsDir + '/style_transfer_enet_inspired/model.png', True)
+    model = style_transfer_conv_inception_ELU_flattened()
+    export_model(model, resultsDir + '/style_transfer_conv_inception_ELU_flattened')
+    plot_model(model, resultsDir + '/style_transfer_conv_inception_ELU_flattened/model.png', True)
+
+    # model = style_transfer_enet_inspired()
+    # export_model(model, resultsDir + '/style_transfer_enet_inspired')
+    # plot_model(model, resultsDir + '/style_transfer_enet_inspired/model.png', True)
     
